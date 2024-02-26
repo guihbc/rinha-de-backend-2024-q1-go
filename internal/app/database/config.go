21aea312582c2004db56918a5205025a7c545917
Guilherme Batista <guihchavesb@gmail.com>
2024-02-26 18:53:14 -0300

    remove logs

--- internal/app/database/config.go
+++ internal/app/database/config.go
@@ -1,12 +1,10 @@
 package database
 
 import (
-	"context"
 	"log"
 	"os"
 	"time"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -16,26 +14,12 @@ func config() *pgxpool.Config {
 		log.Fatalln("Failed to parse pool config")
 	}
 
-	databaseConfig.MaxConns = int32(156)
+	databaseConfig.MaxConns = int32(256)
 	databaseConfig.MinConns = int32(10)
 	databaseConfig.MaxConnLifetime = time.Hour
 	databaseConfig.MaxConnIdleTime = time.Minute
 	databaseConfig.HealthCheckPeriod = time.Second * 30
 	databaseConfig.ConnConfig.ConnectTimeout = time.Second * 5
-
-	databaseConfig.BeforeAcquire = func(ctx context.Context, c *pgx.Conn) bool {
-		log.Println("Before Acquiring the connection pool")
-		return true
-	}
-
-	databaseConfig.AfterRelease = func(c *pgx.Conn) bool {
-		log.Println("After releasing the connection pool")
-		return true
-	}
-
-	databaseConfig.BeforeClose = func(c *pgx.Conn) {
-		log.Println("Closed the connection")
-	}
 
 	return databaseConfig
 }
